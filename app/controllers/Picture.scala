ba9db477165ff6044ae88cd1594fa05c0bff7005
shoma2da <marcos.shoichi.matsuda@gmail.com>
2014-10-09 21:46:29 +0900

    駅名を固定

--- app/controllers/Picture.scala
+++ app/controllers/Picture.scala
@@ -7,8 +7,8 @@ object Picture extends Controller {
 
     def pictures(from:Option[String], to:Option[String]) = Action {
         (from, to) match {
-            case (Some(fromString), Some(toString)) => {
-                Ok(s"OK! Your input '${fromString}' as 'from', '${toString}' as 'to'")
+            case (Some(fromString), Some(toString)) if fromString == "明治神宮前" && toString == "表参道" => { //FIXME 駅名が固定！
+                Ok(s"OK! You input '${fromString}' as 'from', '${toString}' as 'to'")
             }
             case _ => BadRequest("You should input 'from' and 'to' parameters.")
         }
